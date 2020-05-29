d540c6040808bf7c99acbcf748d0057075525a27
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2020-05-29 11:36:52 +0200

    Filtered indirects

--- src/views/MainFeed.vue
+++ src/views/MainFeed.vue
@@ -24,8 +24,7 @@
 <script>
 // @ is an alias to /src
 import Indirect from "@/components/Indirect.vue";
-import { socketOperations } from "../socket/socket";
-import { mapState, mapActions, mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   name: "MainFeed",
@@ -40,9 +39,7 @@ export default {
   methods: {
     ...mapActions("account", ["logout"]),
     ...mapActions("indirect", ["loadIndirects"]),
-    clickButton: function(data) {
-      this.socket.emit("getUserName", null);
-    },
+
     clickDisconect: function(data) {
       this.logout();
     },
@@ -52,46 +49,6 @@ export default {
   },
   data() {
     return {
-      socket: socketOperations.getSocket(),
-      userConected: "eeee",
-
-      indirects2: [
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          date: "20/11/2019",
-          userImg:
-            "https://m.eldiario.es/cultura/persona-pixelada_EDIIMA20160915_0571_18.jpg"
-        },
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          date: "20/11/2019",
-          userImg:
-            "https://m.eldiario.es/cultura/persona-pixelada_EDIIMA20160915_0571_18.jpg"
-        },
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          date: "20/11/2019",
-          userImg:
-            "https://m.eldiario.es/cultura/persona-pixelada_EDIIMA20160915_0571_18.jpg"
-        },
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          date: "20/11/2019",
-          userImg:
-            "https://m.eldiario.es/cultura/persona-pixelada_EDIIMA20160915_0571_18.jpg"
-        },
-        {
-          text:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-          date: "20/11/2019",
-          userImg:
-            "https://m.eldiario.es/cultura/persona-pixelada_EDIIMA20160915_0571_18.jpg"
-        }
-      ]
     };
   },
   created() {
@@ -101,11 +58,6 @@ export default {
     });*/
   },
   mounted() {
-    this.socket.on("setUserName", data => {
-      //console.log(data);
-      //   this.userConected = data
-    });
-
     this.loadIndirects();
   }
 
